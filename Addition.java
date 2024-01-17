aa7b628cfdcae21585f07b4d9c82812b97d1d2f6
Dhivya <dhivya8vivek@gmail.com>
2024-01-17 13:51:23 +0000

    Test change

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition
+{
+public static void main(String args[])
+{
+System.out.println("Simple Addition");
+int a=10,b=90;
+int result=a+b;
+System.out.println("Result is:"+result);
+System.out.println("Simple Subtraction");
+int result2=a-b;
+System.out.println("Result is:"+result2);
+}
+}
